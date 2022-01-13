2fe55e3c8fbe3b3d75c7242f93618495340879c9
AJ <ajabad.dev@gmail.com>
2022-01-13 19:37:24 +0800

    Added error message enum

---
+++ app/modules/errormessages.ts
@@ -0,0 +1,8 @@
+export const errorMessage = {
+  auth: {
+    invalidEmail: "Please enter a valid email address.",
+    invalidPassword: "Please enter a valid password.",
+    invalidCredentials: "Invalid credentials.",
+    emailInUse: "This email address is already in use.",
+  },
+};
--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,13 +1,14 @@
 import Route from "@ioc:Adonis/Core/Route";
 import Database from "@ioc:Adonis/Lucid/Database";
 import { encrypt, decrypt } from "../../app/modules/cryptoutils";
+import { errorMessage } from "../../app/modules/errormessages";
 
 Route.post("auth/checkemail", async (ctx) => {
   const email = ctx.request.input("email")?.toLowerCase().trim();
   const validator = require("email-validator");
   if (!validator.validate(email))
     return ctx.response.badRequest({
-      errorMessage: "Please enter a valid email.",
+      errorMessage: errorMessage.auth.invalidEmail,
     });
   const encryptedEmail = encrypt(email);
 
@@ -20,3 +21,5 @@ Route.post("auth/checkemail", async (ctx) => {
   console.log(emailExists);
   return { emailExists };
 });
+
+Route.post("auth/signup", async (ctx) => {});
