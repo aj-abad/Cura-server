3287b375c7605c743610a620cd7098287d27103e
AJ <ajabad.dev@gmail.com>
2022-01-13 18:39:07 +0800

    Created check email route

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,14 +1,22 @@
 import Route from "@ioc:Adonis/Core/Route";
 import Database from "@ioc:Adonis/Lucid/Database";
+import { encrypt, decrypt } from "../../app/modules/cryptoutils";
 
 Route.post("auth/checkemail", async (ctx) => {
-  const email = ctx.request.input("email");
+  const email = ctx.request.input("email")?.toLowerCase().trim();
   const validator = require("email-validator");
   if (!validator.validate(email))
     return ctx.response.badRequest({
       errorMessage: "Please enter a valid email.",
     });
+  const encryptedEmail = encrypt(email);
 
-  console.log(email);
-  // return Database.from("users").select("*");
+  const emailExists = !!(await Database.from("Users")
+    .where("email", encryptedEmail)
+    .where("UserStatusId", ">", 0)
+    .select(1)
+    .first());
+
+  console.log(emailExists);
+  return { emailExists };
 });
