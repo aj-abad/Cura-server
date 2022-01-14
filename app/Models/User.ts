f2e28f9d4f5ae9a3f86d867e62002c1a604a165c
AJ <ajabad.dev@gmail.com>
2022-01-14 15:53:32 +0800

    Added user ORM model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,43 @@
-import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { BaseModel, column, computed } from "@ioc:Adonis/Lucid/Orm";
+import { decrypt } from "../modules/cryptoutils";
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public userId: string;
+
+  @column()
+  public userStatusId: number;
+
+  @column()
+  public email: string;
+
+  @column()
+  public password: string;
+
+  @column()
+  public firstName: string;
+
+  @column()
+  public lastName: string;
+
+  @column()
+  public mobile: string;
+
+  @computed()
+  public get decryptedEmail(): string {
+    return decrypt(this.email);
+  }
+
+  @computed()
+  public get decryptedMobile(): string {
+    return decrypt(this.mobile);
+  }
+
+  @computed()
+  public get decryptedName(): object {
+    return {
+      firstName: decrypt(this.firstName),
+      lastName: decrypt(this.lastName),
+    };
+  }
 }
