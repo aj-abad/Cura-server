edc3b0d0de39d821d42b3c76a41eb96ad4c62eda
AJ <ajabad.dev@gmail.com>
2022-01-14 15:04:51 +0800

    Changes to signup and signin routes

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,7 +1,8 @@
 import Route from "@ioc:Adonis/Core/Route";
 import Database from "@ioc:Adonis/Lucid/Database";
-import { encrypt, decrypt } from "../../app/modules/cryptoutils";
+import { encrypt, hash } from "../../app/modules/cryptoutils";
 import { errorMessage } from "../../app/modules/errormessages";
+import User from "App/Models/User";
 
 Route.post("auth/checkemail", async (ctx) => {
   const email = ctx.request.input("email")?.toLowerCase().trim();
@@ -22,21 +23,27 @@ Route.post("auth/checkemail", async (ctx) => {
   return { emailExists };
 });
 
-Route.post("auth/signup", async (ctx) => {
-  const email = ctx.request.input("email")?.toLowerCase().trim();
+Route.post("auth/signup", async ({ request, response }) => {
+  const email = request.input("email")?.toLowerCase().trim();
   const validator = require("email-validator");
   if (!validator.validate(email))
-    return ctx.response.badRequest({
+    return response.badRequest({
       errorMessage: errorMessage.auth.invalidEmail,
     });
 
-  const password = ctx.request.input("password");
+  const password = request.input("password");
   if (password.length < 6)
-    return ctx.response.badRequest({
+    return response.badRequest({
       errorMessage: errorMessage.auth.passwordTooShort,
     });
   if (password.length > 128)
-    return ctx.response.badRequest({
+    return response.badRequest({
       errorMessage: errorMessage.auth.passwordTooLong,
     });
+  //TODO actual signup logic
+});
+
+Route.post("auth/signin", async ({ request, response }) => {
+  const email = request.input("email")?.toLowerCase().trim();
+  const password = request.input("password");
 });
