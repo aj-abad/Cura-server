54cc1cc527993c5769fba773f13ac079df714439
AJ <ajabad.dev@gmail.com>
2022-01-14 15:05:05 +0800

    Created function for generating verification codes

---
+++ app/modules/stringutils.ts
@@ -0,0 +1,8 @@
+export function createCode(length: number, includeLetters: boolean): string {
+  let code = "";
+  const possible = "0123456789";
+  for (let i = 0; i < length; i++) {
+    code += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return code;
+}
