590e88b0623011dd5b84b8eb3202221a5799cda4
AJ <ajabad.dev@gmail.com>
2022-01-14 16:09:59 +0800

    Renamed function

--- app/modules/stringutils.ts
+++ app/modules/stringutils.ts
@@ -1,4 +1,4 @@
-export function createCode(length: number, includeLetters: boolean): string {
+export function generateCode(length: number, includeLetters: boolean): string {
   let code = "";
   const possible = "0123456789";
   for (let i = 0; i < length; i++) {
