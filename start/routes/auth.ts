0f768dc94242f28ba545e78c69141003dcf6637c
AJ <ajabad.dev@gmail.com>
2022-01-14 18:39:44 +0800

    Grouped routes and added prefix

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -7,56 +7,57 @@ import { v4 as uuid } from "uuid";
 import { generateCode } from "App/Modules/stringutils";
 import { DateTime } from "luxon";
 
-Route.post("auth/checkemail", async (ctx) => {
-  const email = ctx.request.input("email")?.toLowerCase().trim();
-  const validator = require("email-validator");
-  if (!validator.validate(email))
-    return ctx.response.badRequest({
-      errorMessage: errorMessage.auth.invalidEmail,
-    });
-  const encryptedEmail = encrypt(email);
+Route.group(() => {
+  Route.post("checkemail", async (ctx) => {
+    const email = ctx.request.input("email")?.toLowerCase().trim();
+    const validator = require("email-validator");
+    if (!validator.validate(email))
+      return ctx.response.badRequest({
+        errorMessage: errorMessage.auth.invalidEmail,
+      });
 
-  const emailExists = !!(await Database.from("Users")
-    .where("email", encryptedEmail)
-    .where("UserStatusId", ">", 0)
-    .select(1)
-    .first());
+    const emailExists = !!(await Database.from("Users")
+      .where("email", encrypt(email))
+      .where("UserStatusId", ">", 0)
+      .select(1)
+      .first());
 
-  console.log(emailExists);
-  return { emailExists };
-});
+    return { emailExists };
+  });
 
-Route.post("auth/signup", async ({ request, response }) => {
-  const email = request.input("email")?.toLowerCase().trim();
-  const validator = require("email-validator");
-  if (!validator.validate(email))
-    return response.badRequest({
-      errorMessage: errorMessage.auth.invalidEmail,
-    });
+  Route.post("signup", async ({ request, response }) => {
+    const email = request.input("email")?.toLowerCase().trim();
+    const validator = require("email-validator");
+    if (!validator.validate(email))
+      return response.badRequest({
+        errorMessage: errorMessage.auth.invalidEmail,
+      });
 
-  const password = request.input("password");
-  if (password.length < 6)
-    return response.badRequest({
-      errorMessage: errorMessage.auth.passwordTooShort,
-    });
-  if (password.length > 128)
-    return response.badRequest({
-      errorMessage: errorMessage.auth.passwordTooLong,
-    });
+    const password = request.input("password");
+    if (password.length < 6)
+      return response.badRequest({
+        errorMessage: errorMessage.auth.passwordTooShort,
+      });
+    if (password.length > 128)
+      return response.badRequest({
+        errorMessage: errorMessage.auth.passwordTooLong,
+      });
 
-  const passwordHash = hash(password);
-  const newSignup = new PendingSignup();
-  newSignup.pendingSignupId = uuid();
-  newSignup.email = encrypt(email);
-  newSignup.password = passwordHash;
-  newSignup.code = generateCode(5);
-  newSignup.dateCreated = DateTime.utc();
-  await newSignup.save();
+    const passwordHash = hash(password);
+    const newSignup = new PendingSignup();
+    newSignup.pendingSignupId = uuid();
+    newSignup.email = encrypt(email);
+    newSignup.password = passwordHash;
+    newSignup.code = generateCode(5);
+    newSignup.dateCreated = DateTime.utc();
+    await newSignup.save();
+    //TODO send email
+    return response.created();
+  });
 
-  return response.created();
-});
-
-Route.post("auth/signin", async ({ request, response }) => {
-  const email = request.input("email")?.toLowerCase().trim();
-  const password = request.input("password");
-});
+  Route.post("auth/signin", async ({ request, response }) => {
+    const email = request.input("email")?.toLowerCase().trim();
+    const password = request.input("password");
+    //TODO create sign in logic
+  });
+}).prefix("auth");
