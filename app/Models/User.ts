d7d4f6e6593a1f1ec65316b42914eae13228bee0
AJ <ajabad.dev@gmail.com>
2022-01-15 20:07:03 +0800

    Added decorators to user model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,9 +1,19 @@
-import { BaseModel, column, computed } from "@ioc:Adonis/Lucid/Orm";
+import {
+  BaseModel,
+  column,
+  beforeSave,
+  beforeCreate,
+  beforeFind,
+  afterFetch,
+} from "@ioc:Adonis/Lucid/Orm";
+import Encryption from "@ioc:Adonis/Core/Encryption";
 import { DateTime } from "luxon";
-import { decrypt } from "../modules/cryptoutils";
-
+import Hash from "@ioc:Adonis/Core/Hash";
+import { v4 as uuid } from "uuid";
 export default class User extends BaseModel {
-  @column({ isPrimary: true })
+  @column({
+    isPrimary: true,
+  })
   public userId: string;
 
   @column()
@@ -12,42 +22,73 @@ export default class User extends BaseModel {
   @column()
   public userTypeId: number;
 
-  @column()
   public email: string;
 
-  @column()
+  @column({ serializeAs: null })
   public password: string;
 
-  @column()
+  @column({
+    prepare: (name: string) => Encryption.encrypt(name.trim()),
+    consume: (name: string) => Encryption.decrypt(name),
+  })
   public firstName: string;
 
-  @column()
+  @column({
+    prepare: (name: string) => Encryption.encrypt(name.trim()),
+    consume: (name: string) => Encryption.decrypt(name),
+  })
   public lastName: string;
 
-  @column()
+  @column({
+    prepare: (mobile: string) => Encryption.encrypt(mobile.trim()),
+    consume: (mobile: string) => Encryption.decrypt(mobile),
+  })
   public mobile: string;
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({
+    autoCreate: true,
+    serialize: (date) => date.toString(),
+  })
   public dateRegistered: DateTime;
 
-  @column.dateTime({ autoUpdate: true })
+  @column.dateTime({ autoUpdate: true, serialize: (date) => date.toString() })
   public dateUpdated: DateTime;
 
-  @computed()
-  public get decryptedEmail(): string {
-    return decrypt(this.email);
+  @beforeCreate()
+  public static async generateId(user: User) {
+    user.userId = uuid();
   }
 
-  @computed()
-  public get decryptedMobile(): string {
-    return decrypt(this.mobile);
+  @beforeFind()
+  public static async excludeDeleted(query: any) {
+    query.where("UserStatusId", ">", 0);
   }
 
-  @computed()
-  public get decryptedName(): object {
-    return {
-      firstName: decrypt(this.firstName),
-      lastName: decrypt(this.lastName),
-    };
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password);
+    }
+  }
+
+  @beforeSave()
+  public static async encryptValues(user: User) {
+    if (user.$dirty.firstName) {
+      user.firstName = Encryption.encrypt(user.firstName.trim());
+    }
+    if (user.$dirty.lastName) {
+      user.lastName = Encryption.encrypt(user.lastName.trim());
+    }
+    if (user.$dirty.mobile) {
+      user.mobile = Encryption.encrypt(user.mobile.trim());
+    }
+  }
+  @afterFetch()
+  public static async decryptValues(users[]: User[]){
+    users.forEach((user:User)=>{
+      user.firstName = Encryption.decrypt(user.firstName)!;
+      user.lastName = Encryption.decrypt(user.lastName)!;
+      user.mobile = Encryption.decrypt(user.mobile)!;
+    })
   }
 }
