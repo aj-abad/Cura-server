6a43eadf6436fad7989d43f4abc523d894bd6660
AJ <ajabad.dev@gmail.com>
2022-01-17 21:43:03 +0800

    Configured auth

--- start/routes.ts
+++ start/routes.ts
@@ -20,9 +20,7 @@
 
 import Route from "@ioc:Adonis/Core/Route";
 
-Route.get("*", ({ view }) => {
-  // ctx.response.badRequest({ errorMessage: "Invalid" });
-  // return { hello: "world" };
+Route.get("*", async ({ view }) => {
   return view.render("hutao");
 });
 
