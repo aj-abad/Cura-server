6a4edbeaa293f31760d8d3730545d02860f309f1
AJ <ajabad.dev@gmail.com>
2022-01-19 17:36:42 +0800

    Fixed conflict with Error name

--- app/modules/ErrorMessage.ts
+++ app/modules/ErrorMessage.ts
@@ -1,15 +1,15 @@
 export default class ErrorMessage {
-  public static Error(errorMessage: string): object {
+  public static err(errorMessage: string): object {
     return { errorMessage };
   }
 
   public static Auth = {
-    InvalidEmail: Error("Please enter a valid email address."),
-    InvalidCredentials: Error("Invalid credentials."),
-    PasswordTooShort: Error("Password must be at least 6 characters."),
-    PasswordTooLong: Error("Password is too long."),
-    UserTypeMismatch: Error("This account is for a different Cura app."),
-    EmailInUse: Error("This email address is already in use."),
-    CodeInvalid: Error("This code is expired or invalid."),
+    InvalidEmail: this.err("Please enter a valid email address."),
+    InvalidCredentials: this.err("Invalid credentials."),
+    PasswordTooShort: this.err("Password must be at least 6 characters."),
+    PasswordTooLong: this.err("Password is too long."),
+    UserTypeMismatch: this.err("This account is for a different Cura app."),
+    EmailInUse: this.err("This email address is already in use."),
+    CodeInvalid: this.err("This code is expired or invalid."),
   };
 }
