e733b960cf8c6814e4e7d712cb8d9cc731c33061
AJ <ajabad.dev@gmail.com>
2022-01-19 17:00:43 +0800

    Changed some error messages

--- app/modules/ErrorMessage.ts
+++ app/modules/ErrorMessage.ts
@@ -8,11 +8,8 @@ export default class ErrorMessage {
     InvalidCredentials: Error("Invalid credentials."),
     PasswordTooShort: Error("Password must be at least 6 characters."),
     PasswordTooLong: Error("Password is too long."),
-    UserTypeMismatch: Error(
-      "This email address is registered on a different Cura app."
-    ),
+    UserTypeMismatch: Error("This account is for a different Cura app."),
     EmailInUse: Error("This email address is already in use."),
-    CodeExpired: Error("This verification code has expired."),
-    CodeInvalid: Error("This verification code is invalid."),
+    CodeInvalid: Error("This code is expired or invalid."),
   };
 }
