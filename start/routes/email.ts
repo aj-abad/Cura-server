f886695ca950411ef5105c99bf090607dda752a5
AJ <ajabad.dev@gmail.com>
2022-01-19 19:47:11 +0800

    Added resend email route

--- start/routes/email.ts
+++ start/routes/email.ts
@@ -1,41 +1,53 @@
 import Route from "@ioc:Adonis/Core/Route";
 import Env from "@ioc:Adonis/Core/Env";
-import { validateEmail } from "App/modules/validationutils";
-import { errorMessage } from "App/modules/errormessages";
-import PendingSignup from "App/Models/PendingSignup";
+import { validateEmail } from "App/Modules/validationutils";
+import ErrorMessage from "App/Modules/ErrorMessage";
 import { DateTime } from "luxon";
-import { generateCode } from "App/modules/stringutils";
-import { EmailUtils } from "App/modules/emailutils";
+import { generateCode } from "App/Modules/stringutils";
+import { EmailUtils } from "App/Modules/emailutils";
+import Redis from "@ioc:Adonis/Addons/Redis";
+import PendingSignup from "App/Models/Redis/PendingSignup";
 
 Route.group(() => {
   Route.post("resend", async ({ request, response }) => {
     const email = request.input("email")?.toLowerCase().trim();
-    if (!validateEmail(email))
-      return response.badRequest({
-        errorMessage: errorMessage.auth.invalidEmail,
-      });
+    if (!validateEmail(email)) return response.badRequest();
     //TODO test this
-    const pendingSignup = await PendingSignup.findBy("Email", email);
-    if (!pendingSignup) return null;
-    const codeSentSince = DateTime.utc()
-      .minus({ minutes: Env.get("VERIFICATION_CODE_COOLDOWN_MINUTES") })
-      .toMillis();
 
-    if (pendingSignup.DateCreated.toMillis() < codeSentSince) {
-      const timeRemaining = Math.round(
-        (DateTime.now().toMillis() - codeSentSince) / 1000
+    const signupKey = `signup:${email}`;
+    const record = await Redis.get(signupKey);
+    if (!record) return response.notFound();
+
+    const codeCooldown = Env.get(
+      "VERIFICATION_CODE_COOLDOWN_MINUTES"
+    ) as number;
+    const signupRecord = <PendingSignup>JSON.parse(record);
+    const isSentWithinCooldown =
+      DateTime.utc().toMillis() - signupRecord.DateCreated <
+      codeCooldown * 60 * 1000;
+
+    //If code was sent recently return too many requests
+    if (isSentWithinCooldown) {
+      const secondsBeforeResend = Math.round(
+        codeCooldown * 60 -
+          (DateTime.utc().toMillis() - signupRecord.DateCreated) / 1000
       );
-      return response.tooManyRequests({
-        errorMessage: `Please try again in ${timeRemaining} minutes.`,
-      });
+      return response.tooManyRequests(
+        ErrorMessage.Email.ResendCode(secondsBeforeResend)
+      );
     }
-    pendingSignup.Code = generateCode(5);
-    pendingSignup.DateCreated = DateTime.now();
-    await pendingSignup.save();
-    EmailUtils.sendSignupVerificationMail(
-      pendingSignup.Email,
-      pendingSignup.Code
+
+    //update redis record
+    signupRecord.Code = generateCode(
+      Env.get("VERIFICATION_CODE_LENGTH") as number
     );
-    return response.ok(null);
+    signupRecord.DateCreated = DateTime.utc().toMillis();
+    await Redis.set(signupKey, JSON.stringify(signupRecord));
+    Redis.expire(
+      signupKey,
+      (Env.get("VERIFICATION_CODE_EXPIRY_MINUTES") as number) * 60
+    );
+    //send email
+    EmailUtils.sendSignupVerificationMail(email, signupRecord.Code);
   });
 }).prefix("email");
