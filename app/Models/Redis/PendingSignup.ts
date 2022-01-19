68ac838fd484f226701a13bfe6b18c04e4af7693
AJ <ajabad.dev@gmail.com>
2022-01-19 17:18:31 +0800

    Added constructor to pending sign up

--- app/Models/Redis/PendingSignup.ts
+++ app/Models/Redis/PendingSignup.ts
@@ -1,4 +1,7 @@
 export default class PendingSignup {
+  public constructor(init?: Partial<Person>) {
+    Object.assign(this, init);
+  }
   public Email: string;
   public Password: string;
   public Code: string;
