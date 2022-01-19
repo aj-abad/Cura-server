bb174249ad7f7b3ae0441737a56b671fe35b7d29
AJ <ajabad.dev@gmail.com>
2022-01-19 16:47:41 +0800

    Updated ErrorMessage references

--- start/routes/auth.ts
+++ start/routes/auth.ts
@@ -1,15 +1,15 @@
 import Route from "@ioc:Adonis/Core/Route";
 import Env from "@ioc:Adonis/Core/Env";
 import { DateTime } from "luxon";
-import { errorMessage } from "App/Modules/errormessages";
+import ErrorMessage from "App/modules/ErrorMessage";
 import { generateCode } from "App/Modules/stringutils";
 import { EmailUtils } from "App/Modules/emailutils";
 import { validateEmail } from "App/Modules/validationutils";
 import { UserType } from "App/Enums/UserType";
-import PendingSignup from "App/Models/PendingSignup";
+import PendingSignup from "App/Models/Redis/PendingSignup";
 import User from "App/Models/User";
-import Database from "@ioc:Adonis/Lucid/Database";
 import Hash from "@ioc:Adonis/Core/Hash";
+import Redis from "@ioc:Adonis/Addons/Redis";
 
 Route.group(() => {
   Route.post("checkemail", async ({ request, response }) => {
@@ -18,11 +18,8 @@ Route.group(() => {
     if (!Object.values(UserType).includes(userType)) {
       return response.badRequest();
     }
-
     if (!validateEmail(email))
-      return response.badRequest({
-        errorMessage: errorMessage.auth.invalidEmail,
-      });
+      return response.badRequest(ErrorMessage.Auth.InvalidEmail);
 
     const matchedUser = await User.findBy("Email", email);
     const emailExists = !!matchedUser;
@@ -31,9 +28,7 @@ Route.group(() => {
     }
 
     if (matchedUser.UserTypeId !== userType) {
-      return response.badRequest({
-        errorMessage: errorMessage.auth.userTypeMismatch,
-      });
+      return response.badRequest(ErrorMessage.Auth.PasswordTooShort);
     }
     return { emailExists };
   });
@@ -42,97 +37,65 @@ Route.group(() => {
     const email = request.input("email")?.toLowerCase().trim();
     const password = request.input("password");
     if (!validateEmail(email))
-      return response.badRequest({
-        errorMessage: errorMessage.auth.invalidEmail,
-      });
-
-    if (password.length < 6)
-      return response.badRequest({
-        errorMessage: errorMessage.auth.passwordTooShort,
-      });
-    if (password.length > 128)
-      return response.badRequest({
-        errorMessage: errorMessage.auth.passwordTooLong,
-      });
-
+      return response.badRequest(ErrorMessage.Auth.InvalidEmail);
+    if (password?.length < 6)
+      return response.badRequest(ErrorMessage.Auth.PasswordTooShort);
+    if (password?.length > 128)
+      return response.badRequest(ErrorMessage.Auth.PasswordTooLong);
+    const passwordHash = await Hash.make(password);
     const existingUser = await User.findBy("Email", email);
-    if (!!existingUser)
-      return response.conflict({ errorMessage: errorMessage.auth.emailInUse });
+    if (!!existingUser) return response.conflict(ErrorMessage.Auth.EmailInUse);
 
-    const codeSentSince = DateTime.utc()
-      .minus({ minutes: Env.get("VERIFICATION_CODE_COOLDOWN_MINUTES") })
-      .toMillis();
-
-    const existingPendingSignup = await PendingSignup.query()
-      .where("Email", email)
-      .first();
-
-    if (
-      existingPendingSignup &&
-      existingPendingSignup.DateCreated.toMillis() >= codeSentSince
-    ) {
-      console.log(existingPendingSignup);
-      //only update password if code is still valid
-      existingPendingSignup.Password = password;
-      await existingPendingSignup.save();
-      return response.ok(null);
+    //Check if user has pending sign up on Redis
+    const existingPendingSignup = await Redis.get(`signup:${email}`);
+    if (!existingPendingSignup) {
+      //Create pending sign up record on Redis, make it valid for code_expiry minutes
+      const newUser = {
+        email,
+        password: passwordHash,
+        code: generateCode(5),
+        dateCreated: DateTime.utc().toMillis(),
+      };
+      Redis.set(`signup:${email}`, JSON.stringify(newUser));
+      Redis.expire(
+        `signup:${email}`,
+        Env.get("VERIFICATION_CODE_EXPIRY_MINUTES") * 60
+      );
+      EmailUtils.sendSignupVerificationMail(email, newUser.code);
+      return response.created();
     }
-    //otherwise delete old sign up records if any and create new
-    await Database.from("PendingSignups").where("email", email).delete();
-    //then send an email
-    const code = generateCode(5);
-    await new PendingSignup()
-      .merge({
-        Email: email,
-        Password: password,
-        Code: code,
-      })
-      .save();
-    EmailUtils.sendSignupVerificationMail(email, code);
-  });
+    //TODO Update user pending signup record if exists
 
-  Route.post("verify", async ({ auth, request, response }) => {
-    const code = request.input("code");
-    const email = request.input("email")?.toLowerCase().trim();
-    //Find user to verify
-    const toVerify = await PendingSignup.query()
-      .where("Email", email)
-      .andWhere("Code", code)
-      .first();
-
-    //if no matching record found
-    if (!toVerify)
-      return response.unauthorized({
-        errorMessage: errorMessage.auth.codeInvalid,
-      });
-
-    //if code is expired
+    const existingSignup: PendingSignup = <PendingSignup>(
+      JSON.parse(existingPendingSignup)
+    );
+    existingSignup.Password = passwordHash;
+    Redis.expire(
+      `signup:${email}`,
+      Env.get("VERIFICATION_CODE_EXPIRY_MINUTES") * 60
+    );
     if (
-      toVerify.DateCreated.toMillis() <
+      existingSignup.DateCreated <
       DateTime.utc()
-        .minus({ minutes: Env.get("VERIFICATION_CODE_EXPIRY_MINUTES") })
+        .minus({ minutes: Env.get("VERIFICATION_CODE_COOLDOWN_MINUTES") })
         .toMillis()
     ) {
-      return response.badRequest({
-        errorMessage: errorMessage.auth.codeExpired,
-      });
+      Redis.set(`signup:${email}`, JSON.stringify(existingSignup));
+      Redis.expire(
+        `signup:${email}`,
+        Env.get("VERIFICATION_CODE_EXPIRY_MINUTES")
+      );
+      return response.ok(null);
     }
-
-    const { Email, Password } = toVerify;
-
-    //create user and delete pending signup record
-    await Database.from("PendingSignups").where("email", Email).delete();
-    const newUser = new User();
-    newUser.shouldHashPassword = false;
-    newUser.merge({
-      Email,
-      Password,
-      UserStatusId: 2,
-      UserTypeId: 1,
-    });
-
-    await newUser.save();
-    return response.created(await auth.use("api").generate(newUser));
+    //otherwise send verification code
+    existingSignup.Code = generateCode(5);
+    existingSignup.DateCreated = DateTime.utc().toMillis();
+    Redis.set(`signup:${email}`, JSON.stringify(existingSignup));
+    Redis.expire(
+      `signup:${email}`,
+      Env.get("VERIFICATION_CODE_EXPIRY_MINUTES")
+    );
+    EmailUtils.sendSignupVerificationMail(email, existingSignup.Code);
   });
 
   Route.post("signin", async ({ auth, request, response }) => {
@@ -142,17 +105,13 @@ Route.group(() => {
     //Find matching user
     const user = await User.findBy("Email", email);
     if (!user)
-      return response.unauthorized({
-        errorMessage: errorMessage.auth.invalidCredentials,
-      });
+      return response.unauthorized(ErrorMessage.Auth.InvalidCredentials);
 
     //Get stuff from user
     const { UserStatusId: userStatus } = user;
     const isPasswordValid = await Hash.verify(user.Password, password);
     if (!isPasswordValid) {
-      return response.unauthorized({
-        errorMessage: errorMessage.auth.invalidCredentials,
-      });
+      return response.unauthorized(ErrorMessage.Auth.InvalidCredentials);
     }
 
     // verified
@@ -162,5 +121,14 @@ Route.group(() => {
       userStatus,
       token,
     };
+  });
+
+  Route.post("verify", async ({ auth, request, response }) => {
+    const code = request.input("code");
+    const email = request.input("email")?.toLowerCase().trim();
+
+    const matchedSignup = await Redis.get(`signup:${email}`);
+    if (!matchedSignup)
+      return response.unauthorized(ErrorMessage.Auth.CodeInvalid);
   });
 }).prefix("auth");
