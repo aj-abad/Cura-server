5e72c175fbd638b2dd1ceb4170ebe8c49e452032
AJ <ajabad.dev@gmail.com>
2022-01-19 20:38:42 +0800

    Added validation methods for name, mobile, UUID

--- app/modules/Validation.ts
+++ app/modules/Validation.ts
@@ -4,6 +4,12 @@ export default class Validation {
     return validator.validate(email);
   }
 
+  public static validateUuid(uuid: string): boolean {
+    const uuidPattern =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidPattern.test(uuid);
+  }
+
   public static validateMobileNumber(mobile: string): boolean {
     //valid Philippine mobile number prefixes
     const validPrefixes = [
