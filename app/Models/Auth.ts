172201be7b58f6f9890efdfef4c0509ee6392025
AJ <ajabad.dev@gmail.com>
2022-01-20 11:57:26 +0800

    Reconfigured auth and created dedicated auth model

--- app/Models/Auth.ts
+++ app/Models/Auth.ts
@@ -1,30 +1,11 @@
-import { DateTime } from 'luxon'
-import Hash from '@ioc:Adonis/Core/Hash'
-import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+import { column, BaseModel } from "@ioc:Adonis/Lucid/Orm";
 
 export default class Auth extends BaseModel {
+  public static table = "Users";
+
   @column({ isPrimary: true })
-  public id: number
+  public UserId: number;
 
   @column()
-  public email: string
-
-  @column({ serializeAs: null })
-  public password: string
-
-  @column()
-  public rememberMeToken?: string
-
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
-
-  @beforeSave()
-  public static async hashPassword (auth: Auth) {
-    if (auth.$dirty.password) {
-      auth.password = await Hash.make(auth.password)
-    }
-  }
+  public UserTypeId: number;
 }
