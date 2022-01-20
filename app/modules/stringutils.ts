9abde83d3d2eb2ac6ca8b93f6c8211c49bdd2096
AJ <ajabad.dev@gmail.com>
2022-01-20 13:31:08 +0800

    Renamed string helpers

--- app/modules/stringutils.ts
+++
@@ -1,8 +0,0 @@
-export function generateCode(length: number): string {
-  let code = "";
-  const possible = "0123456789";
-  for (let i = 0; i < length; i++) {
-    code += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return code;
-}
