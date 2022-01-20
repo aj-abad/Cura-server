7ecd1d312e0c1bec8b8ff36c20500d6f723da6ff
AJ <ajabad.dev@gmail.com>
2022-01-20 19:25:11 +0800

    Updated EmailUtils class

--- app/modules/emailutils.ts
+++ app/modules/emailutils.ts
@@ -1,4 +1,6 @@
 import Env from "@ioc:Adonis/Core/Env";
+import { DateTime } from "luxon";
+
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(Env.get("SENDGRID_API_KEY"));
 
@@ -7,18 +9,20 @@ const from = {
   name: Env.get("SENDGRID_SENDER_NAME"),
 };
 
-export class EmailUtils {
+export default class EmailUtils {
   public static sendSignupVerificationMail(
     recipient: string,
     code: string
   ): boolean {
     let isSent: boolean = false;
+    const year = DateTime.utc().year;
     const msg = {
       to: recipient,
       from,
       templateId: "d-9e13297c99504d71ae41dea203a38c88",
       dynamicTemplateData: {
         code,
+        year,
       },
     };
     sgMail
