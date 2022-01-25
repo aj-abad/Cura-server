13d6b20db00e96e43f805e0b3ac06afa16466fdd
AJ <ajabad.dev@gmail.com>
2022-01-25 19:43:50 +0800

    TODO: authenticate user in socket

--- start/socket.ts
+++ start/socket.ts
@@ -1,10 +1,13 @@
 import Ws from "App/Services/Ws";
+
 Ws.boot();
 
 /**
  * Listen for incoming socket connections
  */
 Ws.io.on("connection", (socket) => {
+  const { token } = socket.handshake.auth;
+  //TODO authenticate user
   socket.emit("news", { hello: "world" });
 
   socket.on("my other event", (data) => {
