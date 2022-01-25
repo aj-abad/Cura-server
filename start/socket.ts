da1d4f7dc3822a9433e878ce219a34e226290325
AJ <ajabad.dev@gmail.com>
2022-01-25 21:50:34 +0800

    Added socket auth

--- start/socket.ts
+++ start/socket.ts
@@ -1,15 +1,18 @@
 import Ws from "App/Services/Ws";
-
+import socketAuthenticate from "App/Modules/SocketAuth";
+import User from "App/Models/User";
 Ws.boot();
 
 /**
  * Listen for incoming socket connections
  */
-Ws.io.on("connection", (socket) => {
+Ws.io.on("connection", async (socket) => {
   const { token } = socket.handshake.auth;
-  //TODO authenticate user
-  socket.emit("news", { hello: "world" });
+  const user: User = await socketAuthenticate(token);
+  if (!user) return socket.disconnect(true);
+  socket.join(user.UserId);
 
+  socket.emit("news", { hello: "world" });
   socket.on("my other event", (data) => {
     console.log(data);
   });
