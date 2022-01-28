ecf30c8c6a8236a51f82508f34592b3222fa6237
AJ <ajabad.dev@gmail.com>
2022-01-28 13:11:24 +0800

    Made socket auth fail silently

--- app/modules/SocketAuth.ts
+++ app/modules/SocketAuth.ts
@@ -2,12 +2,14 @@ import Redis from "@ioc:Adonis/Addons/Redis";
 import User from "App/Models/User";
 import { base64 } from "@ioc:Adonis/Core/Helpers";
 
-export default async function socketAuthenticate(token: string): Promise<User> {
+export default async function socketAuthenticate(
+  token: string
+): Promise<User | null> {
   const encodedTokenId = token?.replace("Bearer ", "")?.split(".")?.[0];
-  if (!encodedTokenId) throw new Error("Invalid token.");
+  if (!encodedTokenId) return null;
   const tokenId = base64.urlDecode(encodedTokenId);
   const userStr = await Redis.get(`api:${tokenId}`);
-  if (!userStr) throw new Error("Invalid token.");
+  if (!userStr) return null;
   const { user_id: userId } = JSON.parse(userStr);
-  return User.findOrFail(userId);
+  return User.find(userId);
 }
