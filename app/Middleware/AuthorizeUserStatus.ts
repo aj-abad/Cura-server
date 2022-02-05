9f6bac22be10497027cbc9ada1a089dd9f09bb13
AJ <ajabad.dev@gmail.com>
2022-02-05 20:03:59 +0800

    Fixed type error on user status middleware

--- app/Middleware/AuthorizeUserStatus.ts
+++ app/Middleware/AuthorizeUserStatus.ts
@@ -4,11 +4,14 @@ export default class AuthorizeUserStatus {
   public async handle(
     { auth, response }: HttpContextContract,
     next: () => Promise<void>,
-    authorizedStatus: number[]
+    authorizedStatus: any[]
   ) {
+    const authorized: number[] = authorizedStatus.map((status) => parseInt(status.toString()));
     const { UserStatusId } = auth.use("api").user!;
-    if (!authorizedStatus.includes(UserStatusId)) {
-      return response.unauthorized();
+    if (!authorized.includes(UserStatusId)) {
+      return response.unauthorized({
+        errorMessage: "You are not authorized to perform this action",
+      });
     }
     await next();
   }
