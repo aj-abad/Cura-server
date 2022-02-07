abb4cb31ee704753c806353593b2ae348e96ffb9
AJ <ajabad.dev@gmail.com>
2022-02-07 17:35:04 +0800

    Added route for getting current user details

--- start/routes/account.ts
+++ start/routes/account.ts
@@ -1,12 +1,14 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
+  Route.get("details", "AccountController.getUserDetails").middleware(["auth"]);
+  
   Route.post("setup", "AccountController.setup").middleware([
     "auth:api",
     "authorizeUserStatus:2",
     "validateAndSanitizeEmail",
   ]);
-
+  
   Route.post("resetpassword", "AccountController.resetPassword").middleware([
     "validateAndSanitizeEmail",
   ]);
