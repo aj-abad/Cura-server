f86a392f85bed02c6fbb00715b9dace3eae7adc4
AJ <ajabad.dev@gmail.com>
2022-02-08 11:04:46 +0800

    Changed UserId type to string

--- app/Models/Auth.ts
+++ app/Models/Auth.ts
@@ -4,7 +4,7 @@ export default class Auth extends BaseModel {
   public static table = "Users";
 
   @column({ isPrimary: true })
-  public UserId: number;
+  public UserId: string;
 
   @column()
   public UserTypeId: number;
